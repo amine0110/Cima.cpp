6c445914b5ef0a6ed85c7a32fe0c1b104ef492e6
Mokhtari Mohammed El Amine <37108394+amine0110@users.noreply.github.com>
2020-01-10 23:22:26 +0100

    Update Ima.cpp

--- Ima.cpp
+++ Ima.cpp
@@ -98,83 +98,162 @@ void CIma::ConvertRGBYUY2(BYTE *ptrIn, BYTE *pyuv, int Larg, int Haut)
 
 }
 
-void CIma::Contour(unsigned char * ImrgbPtr, int Larg, int Haut)
+void CIma::Contour(unsigned char * ImagTrait, int Larg, int Haut)
 {
-	int colorIdx = 0;	
-	int x00, x01, x10, x11;
+	int Coul = 0;	
+	int P11, P12, P21, P22;
 	for (int i = 0; i <Haut - 1; i++) {
 		for (int j = 0; j < Larg - 1; j++) {
 
-		for (colorIdx = 0; colorIdx < 3; colorIdx++) {
-				x00 = (i * Larg + j) * 3 + colorIdx;	
-				x01 = (i * Larg + (j+1)) * 3 + colorIdx;
-				x10 = ((i+1) * Larg  + j) * 3 + colorIdx;
-				x11 = ((i+1) * Larg + (j+1)) * 3 + colorIdx;
+		for (Coul = 0; Coul < 3; Coul++) {
+				P11 = (i * Larg + j) * 3 + Coul;	
+				P12 = (i * Larg + (j+1)) * 3 + Coul;
+				P21 = ((i+1) * Larg  + j) * 3 + Coul;
+				P22 = ((i+1) * Larg + (j+1)) * 3 + Coul;
 				
-				ImrgbPtr[x00] = (int) sqrt((ImrgbPtr[x00] - ImrgbPtr[x11])*(ImrgbPtr[x00] - ImrgbPtr[x11])  +  (ImrgbPtr[x01] - ImrgbPtr[x10])*(ImrgbPtr[x01] - ImrgbPtr[x10]));
+				ImagTrait[P11] = (int) sqrt((ImagTrait[P11] - ImagTrait[P22])*(ImagTrait[P11] - ImagTrait[P22])  +  (ImagTrait[P12] - ImagTrait[P21])*(ImagTrait[P12] - ImagTrait[P21]));
 				
 			}
 		}
 	}
 }
 
-void CIma::Contour2(unsigned char * ImrgbPtr, int Larg, int Haut)
+void CIma::Contour2(unsigned char * ImagTrait, int Larg, int Haut)
 {
-	int colorIdx = 0;
-	int x00, x01, x10, x11, x02, x12, x20, x21, x22;
+	int Coul = 0;
+	int P11, P12, P21, P22, P13, x12, P31, P32, P33;
 	for (int i = 0; i < Haut - 1; i++) {
 		for (int j = 0; j < Larg - 1; j++) {
 
-			for (colorIdx = 0; colorIdx < 3; colorIdx++) {
-				x00 = (i * Larg + j) * 3 + colorIdx;
-				x01 = (i * Larg + (j + 1)) * 3 + colorIdx;
-				x10 = ((i + 1) * Larg + j) * 3 + colorIdx;
-				x11 = ((i + 1) * Larg + (j + 1)) * 3 + colorIdx;
-				x02 = ((i)* Larg + (j + 2)) * 3 + colorIdx;
-				x12 = ((i + 1) * Larg + (j + 2)) * 3 + colorIdx;
-				x20 = ((i + 2) * Larg + (j)) * 3 + colorIdx;
-				x21 = ((i + 2) * Larg + (j + 1)) * 3 + colorIdx;
-				x22 = ((i + 2) * Larg + (j + 2)) * 3 + colorIdx;
+			for (Coul = 0; Coul < 3; Coul++) {
+				P11 = (i * Larg + j) * 3 + Coul;
+				P12 = (i * Larg + (j + 1)) * 3 + Coul;
+				P21 = ((i + 1) * Larg + j) * 3 + Coul;
+				P22 = ((i + 1) * Larg + (j + 1)) * 3 + Coul;
+				P13 = ((i)* Larg + (j + 2)) * 3 + Coul;
+				x12 = ((i + 1) * Larg + (j + 2)) * 3 + Coul;
+				P31 = ((i + 2) * Larg + (j)) * 3 + Coul;
+				P32 = ((i + 2) * Larg + (j + 1)) * 3 + Coul;
+				P33 = ((i + 2) * Larg + (j + 2)) * 3 + Coul;
 				
-				ImrgbPtr[x00] = (int)sqrt(ImrgbPtr[x00] + ImrgbPtr[x10] + ImrgbPtr[x22] - ImrgbPtr[x02] - ImrgbPtr[x12] - ImrgbPtr[x22]);
+				ImagTrait[P11] = (int)sqrt(ImagTrait[P11] + ImagTrait[P21] + ImagTrait[P33] - ImagTrait[P13] - ImagTrait[x12] - ImagTrait[P33]);
 			}
 		}
 	}
 }
 
-void CIma::inversion(unsigned char * ImrgbPtr, int Larg, int Haut)
+void CIma::inversion(unsigned char * ImagTrait, int Larg, int Haut)
 {
-	int colorIdx = 0;
-	int x00, x01, x10, x11;
+	int Coul = 0;
+	int P11, P12, P21, P22;
 	for (int i = 0; i < Haut - 1; i++) {
 		for (int j = 0; j < Larg - 1; j++) {
 
-			for (colorIdx = 0; colorIdx < 3; colorIdx++) {
-				x00 = (i * Larg + j) * 3 + colorIdx;
-				ImrgbPtr[x00] = 255 - ImrgbPtr[x00];
+			for (Coul = 0; Coul < 3; Coul++) {
+				P11 = (i * Larg + j) * 3 + Coul;
+				ImagTrait[P11] = 255 - ImagTrait[P11];
 			}
 		}
 	}
-
-
-
 }
 
-void CIma::seuillage(unsigned char * ImrgbPtr, int Larg, int Haut)
+void CIma::seuillage(unsigned char * ImagTrait, int Larg, int Haut)
 {
-	int colorIdx = 0;
-	int x00, x01, x10, x11;
+	int Coul = 0;
+	int P11, P12, P21, P22;
 	for (int i = 0; i < Haut - 1; i++) {
 		for (int j = 0; j < Larg - 1; j++) {
 
-			for (colorIdx = 0; colorIdx < 3; colorIdx++) {
-				x00 = (i * Larg + j) * 3 + colorIdx;
-				if (ImrgbPtr[x00] > 130) {
-					ImrgbPtr[x00] = 255;
+			for (Coul = 0; Coul < 3; Coul++) {
+				P11 = (i * Larg + j) * 3 + Coul;
+				if (ImagTrait[P11] > 130) {
+					ImagTrait[P11] = 255;
 				}
-				else ImrgbPtr[x00] = 0;
+				else ImagTrait[P11] = 0;
 			}
 		}
 	}
 
 }
+unsigned char * ImagTrait2;
+void CIma::rotation(unsigned char * ImagTrait, int Larg, int Haut)
+{
+	
+	int In, Jn,i,j, tita = 3.1415, Tx = Larg/2, Ty = Haut/2;
+	int Coul = 0, k=0;
+	int P11, P11n;
+	ImagTrait2 = (unsigned char *)malloc(Larg * Haut * 3 * sizeof(unsigned char *));
+	
+	for (int In = 0; In < Haut - 1; In++) {
+		for (int Jn = 0; Jn < Larg - 1; Jn++) {
+			i =(int)( In * cos(tita) - Jn * sin(tita) + Tx);
+			j = (int)(Jn * sin(tita) + Jn * cos(tita) + Ty);
+			
+				for (Coul = 0; Coul < 3; Coul++) {
+
+					if ((i >= 0 && i <= Larg - 1) && (j >= 0 && j <= Haut - 1))
+					{
+						P11n = (In * Larg + Jn) * 3 + Coul;
+						P11 = (i * Larg + j) * 3 + Coul;
+						ImagTrait2[P11n] = ImagTrait[P11];
+					}
+					else {
+						P11n = (In * Larg + Jn) * 3 + Coul;
+						ImagTrait2[P11n] = 0;
+					}
+					k++;
+
+
+				}
+			
+			
+			
+				
+				
+			
+		}
+	}
+
+	ImagTrait = ImagTrait2;
+
+
+
+}
+
+
+void CIma::Flou(unsigned char * ImagTrait, int Larg, int Haut)
+{
+	int Coul = 0;
+	int k = 0;
+	int P11, P12, P13, P21, P22, P23, P30, P31, P33;
+	while (k < 20)
+	{
+		for (int i = 0; i < Haut - 4; i++) {
+			for (int j = 0; j < Larg - 4; j++) {
+
+				for (Coul = 0; Coul < 3; Coul++) {
+					P11 = (i * Larg + j) * 3 + Coul;
+					P12 = (i * Larg + (j + 1)) * 3 + Coul;
+					P13 = (i * Larg + (j + 2)) * 3 + Coul;
+
+					P21 = ((i + 1) * Larg + j) * 3 + Coul;
+					P22 = ((i + 1) * Larg + (j + 1)) * 3 + Coul;
+					P23 = ((i + 1) * Larg + (j + 2)) * 3 + Coul;
+
+					P30 = ((i + 2) * Larg + j) * 3 + Coul;
+					P31 = ((i + 2) * Larg + (j + 1)) * 3 + Coul;
+					P33 = ((i + 2) * Larg + (j + 2)) * 3 + Coul;
+
+					ImagTrait[P11] = (int)((1 * ImagTrait[P11] + 1 * ImagTrait[P12] + 1 * ImagTrait[P13]
+						+ 1 * ImagTrait[P21] + 1 * ImagTrait[P22] + 1 * ImagTrait[P23]
+						+ 1 * ImagTrait[P30] + 1 * ImagTrait[P31] + 1 * ImagTrait[P33]) / 9);
+				}
+
+
+			}
+		}
+		k++;
+
+	}
+}
+
